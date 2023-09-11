41915e8418e74228a88e4e398729d7e1db846de9
Bruno Bach <brunobach_sl@hotmail.com>
2023-09-11 16:09:17 -0300

    add env prod

--- ui/src/utils/ApiClient.js
+++ ui/src/utils/ApiClient.js
@@ -1,8 +1,8 @@
 import PocketBase from "pocketbase";
 
-const client = new PocketBase(
+const client = import.meta.env.PB_ENV === "dev" ? new PocketBase(
     import.meta.env.PB_BACKEND_URL,
-);
+) : new PocketBase(window.location.href);
 
 
 export default client;
